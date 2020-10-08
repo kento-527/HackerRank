49452880d5a5e468fbf68b95799468d4c3a39a4b
DESKTOP-P993UG9\kento <kento19990527@icloud.com>
2020-10-09 01:01:35 +0900

    プログラミング生活７日目

---
+++ day06.py
@@ -0,0 +1,16 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+num_test_cases=int(input())
+
+for i in range(num_test_cases):
+    test_string=input()
+    even=''
+    odd=''
+
+    for j in range(len(test_string)):
+        if j % 2==0:
+            even+=test_string[j]
+        else:
+            odd+=test_string[j]
+
+    print('{} {}'.format(even,odd))
